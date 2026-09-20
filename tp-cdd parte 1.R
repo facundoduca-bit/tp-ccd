31d22b8812fcab7c74d18cd2a00cc49a3e8e6d21
juampi2532 <duartejuampi25@gmail.com>
2026-09-19 21:11:02 -0300

    agrego tablas y graficos de analisis exploratorio

--- tp-cdd parte 1.R
+++ tp-cdd parte 1.R
@@ -31,6 +31,82 @@ base_unida <- read_csv("base_da_1 (1).csv") %>%
 # chequeo final
 table(base_unida$estado_productivo, useNA = "always")
 
+# Chequear dimensiones
+dim(base_da_1)
+dim(base_da_2)
+dim(base_unida)
+
+# Chequear valores nulos
+colSums(is.na(base_da_1))
+colSums(is.na(base_da_2))
+colSums(is.na(base_unida))
+
+# Chequear que no haya más de una fila por provincia-producto
+base_unida %>%
+  count(cod_provincia, cod_ncm_6d) %>%
+  filter(n > 1)
+
 # !!!COMIENZO ANALISIS EXPORATORIO DE DATOS (PARTE 3 TP)!!!
 
+# 1.Crear una tabla con una sola observacion por provincia
+tabla_provincias <- base_da_2 %>%
+  distinct(provincia, complejidad_provincia)
 
+# 2.Calcular medidas descriptivas de la complejidad provincial
+tabla_provincias %>%
+  summarise(
+    media = mean(complejidad_provincia, na.rm = TRUE),
+    mediana = median(complejidad_provincia, na.rm = TRUE),
+    desvio = sd(complejidad_provincia, na.rm = TRUE),
+    minimo = min(complejidad_provincia, na.rm = TRUE),
+    maximo = max(complejidad_provincia, na.rm = TRUE)
+  )
+# 3. Crear grafico de la complejidad economica de cada provincia
+ggplot(tabla_provincias,
+       aes(x = reorder(provincia, complejidad_provincia),
+           y = complejidad_provincia)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    title = "Complejidad economica por provincia",
+    x = "Provincia",
+    y = "complejidad provincial"
+  )
+
+# 4. Crear una tabla con solo una observacion por producto
+tabla_productos <- base_da_2 %>%
+  distinct(cod_ncm_6d, ncm_6d, seccion, complejidad_producto, centralidad)
+
+# 5. Crea un grafico sobre la distribucion de la complejidad de los productos
+ggplot(
+  tabla_productos %>% filter(!is.na(complejidad_producto)),
+  aes(x = complejidad_producto)
+) +
+  geom_histogram(bins = 30) +
+  labs(
+    title = "Distribucion de la complejidad de los productos",
+    x = "Complejidad del producto",
+    y = "Cantidad de productos"
+  )
+
+# 6. Crear una tabla con la complejidad promedio de los productos por seccion
+complejidad_seccion <- tabla_productos %>%
+  filter(!is.na(complejidad_producto)) %>%
+  group_by(seccion) %>%
+  summarise(
+    complejidad_promedio = mean(complejidad_producto)
+  )
+
+# 7. Generar grafico que compare la complejidad de los productos por secciones
+ggplot(
+  complejidad_seccion,
+  aes(
+    x = reorder(seccion, complejidad_promedio),
+    y = complejidad_promedio)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    title = "Complejidad promedio de los productos por seccion",
+    x = "Seccion",
+    y = "Complejidad promedio"
+  )
